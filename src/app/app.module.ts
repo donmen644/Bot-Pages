84edfc4563aa4dcdc3ab3afb605a93e9778e3c39
donmen644 <darksiguapo.1@gmail.com>
2021-05-01 15:33:57 -0600

    login work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
